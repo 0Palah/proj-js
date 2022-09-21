a7508da48684614f0419d61e8692b1ad11b5ae10
Oleg Palashynskyi <palah@ukr.net>
2022-09-21 21:23:45 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+import { DeveloperApi } from './js/DeveloperApi';
 import Notiflix from 'notiflix';
+// import galleryCard from './templates/gallery-card.hbs';
+
+const searchCountryEl = document.querySelector('#country');
+console.log(searchCountryEl);
+const searchingStrEl = document.querySelector('#searching');
+console.log(searchingStrEl);
+const btnSubmitEl = document.querySelector('.header__form-submit');
+console.log(btnSubmitEl);
+const headerFormEl = document.querySelector('.header__form');
+console.dir(headerFormEl);
+
+const onInputElSubmit = async event => {
+  event.preventDefault();
+  const countryCode = searchCountryEl.value;
+  console.log(countryCode);
+};
+
+headerFormEl.addEventListener('submit', onInputElSubmit);
