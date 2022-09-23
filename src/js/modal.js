411e64756579d289bd737590ede2ccbb586c1ab2
Hudyma_Ihor <Hudyma.work@gmail.com>
2022-09-23 09:23:42 +0300

    close_modal

--- src/js/modal.js
+++ src/js/modal.js
@@ -1,15 +1,36 @@
-'use strict';
-(() => {
-      const refs = {
-        openModalBtn: document.querySelector("[data-modal-open]"),
-        closeModalBtn: document.querySelector("[data-modal-close]"),
-        modal: document.querySelector("[data-modal]"),
-      };
-    
-      refs.openModalBtn.addEventListener("click", toggleModal);
-      refs.closeModalBtn.addEventListener("click", toggleModal);
-    
-      function toggleModal() {
-        refs.modal.classList.toggle("is-hidden");
-      }
-    })();
+    'use strict';
+
+const openModalBtnEl = document.querySelector("[data-modal-open]");
+const backdropEl = document.querySelector("[data-modal]");
+const closeModalBtnEl = document.querySelector("[data-modal-close]");
+
+const onOpenModalBtnElClick = event => {
+  backdropEl.classList.add('is-open');
+
+  window.addEventListener('keydown', onEscBtnPush);
+};
+
+const closeModalWindow = event => {
+  backdropEl.classList.remove('is-open');
+  window.removeEventListener('keydown', onEscBtnPush);
+};
+
+const onEscBtnPush = event => {
+  if (event.code !== 'Escape') {
+    return;
+  }
+
+  closeModalWindow();
+};
+
+const onBackdropElClick = event => {
+  if (event.currentTarget !== event.target) {
+    return;
+  }
+
+  closeModalWindow();
+};
+
+//openModalBtnEl.addEventListener('click', onOpenModalBtnElClick);
+closeModalBtnEl.addEventListener('click', closeModalWindow);
+backdropEl.addEventListener('click', onBackdropElClick);
